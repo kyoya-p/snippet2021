13078ff3162cf962054a2af3b32cb45b3a849068
kyoya <kyoya.p@gmail.com>
2021-01-27 08:05:35 +0900

    n

--- openapiGenerator/src/main/kotlin/main.kt
+++ openapiGenerator/src/main/kotlin/main.kt
@@ -3,12 +3,11 @@ import idpManager.srdm.api.BasicFunctionsApi
 fun main() {
     val basicApi = BasicFunctionsApi()
 
-    val dev1 = idpManager.srdm.model.RegisterDeviceManuallyReqRegisterTargetList
-    (
+    val dev1 = idpManager.srdm.model.RegisterDeviceManuallyReqRegisterTargetList(
         target = "10.36.102.184",
         port = 8086,
         user = "",
-        password = ""
+        password = "",
     )
 
 
