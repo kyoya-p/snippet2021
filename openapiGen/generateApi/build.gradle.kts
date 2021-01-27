8ee969f1c7c64c69dcca07033453b03039809a35
kk <kyoya.p@gmail.com>
2021-01-27 13:08:59 +0900

    n

--- openapiGen/generateApi/build.gradle.kts
+++ openapiGen/generateApi/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") //version "1.4.21"
+    id("org.openapi.generator") version "5.0.0"
 }
 
 group = "org.example"
@@ -11,4 +12,19 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+val apiName = "srdm"
+val buildApiDir = "$buildDir/gen/$apiName"
+val basePackage = "idpManager"
+
+task<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("generateApi") {
+    inputSpec.set("$rootDir/SampleAPI.yaml")
+    generatorName.set("kotlin")
+    library.set("multiplatform")
+    outputDir.set(buildApiDir)
+    packageName.set(basePackage)
+    apiPackage.set("$basePackage.$apiName.api")
+    modelPackage.set("$basePackage.$apiName.model")
+    generateApiTests.set(false)
 }
