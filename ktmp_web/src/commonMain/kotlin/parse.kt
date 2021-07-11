338f7613305d6c9cdad1f585be0cbcab35630dae
kyoya <kyoya.p@gmail.com>
2021-07-12 05:18:06 +0900

    n

--- ktmp_web/src/commonMain/kotlin/parse.kt
+++
@@ -1,8 +0,0 @@
-import io.ktor.client.*
-import io.ktor.client.request.*
-
-suspend fun parse(): String {
-    val url = "https://www.battlespirits.com/shopbattle/list.php?pref=24"
-    val html = HttpClient().get<String>(url)
-    return html
-}
