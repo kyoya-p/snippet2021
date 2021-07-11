f94dc2131e63075a7f1873c8fbfb728458b69be0
kyoya <kyoya.p@gmail.com>
2021-07-11 13:57:17 +0900

    n

--- ftr_maps/lib/main.dart
+++ ftr_maps/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps/google_maps.dart';
 
 void main() {
+//  final mapOptions = MapOptions()
+//    ..zoom = 8
+//    ..center = LatLng(-34.397, 150.644);
+//  GMap(document.getElementById("map-canvas"), mapOptions);
   runApp(MyApp());
 }
 
