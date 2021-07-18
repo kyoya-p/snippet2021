6c03373619bfa77ae461957fe76f8fbc535566c8
kyoya <kyoya.p@gmail.com>
2021-07-19 04:29:27 +0900

    html DSL対応

---
+++ googlemaps/210718_ktjs_html/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.5.20"
+    kotlin("plugin.serialization") version "1.5.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val ktorVersion = "1.6.1"
+dependencies {
+    implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.5.1")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:1.5.20")
+    implementation("io.ktor:ktor-client-json-js:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization-js:$ktorVersion")
+}
+
+kotlin {
+    js {
+        browser {
+            webpackTask {
+                cssSupport.enabled = true
+            }
+
+            runTask {
+                cssSupport.enabled = true
+            }
+
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+
+        }
+        binaries.executable()
+    }
+}
