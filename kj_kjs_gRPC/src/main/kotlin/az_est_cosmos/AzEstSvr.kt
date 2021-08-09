258230e7d2455191e5a16354d52f88980475b278
kyoya <kyoya.p@gmail.com>
2021-08-10 06:00:41 +0900

    Server作成(枠だけ)

--- kj_kjs_gRPC/src/main/kotlin/az_est_cosmos/AzEstSvr.kt
+++ kj_kjs_gRPC/src/main/kotlin/az_est_cosmos/AzEstSvr.kt
@@ -4,22 +4,29 @@ import io.grpc.ServerBuilder
 
 fun main() {
     val port = System.getenv("PORT")?.toInt() ?: 50051
-    val server = ServerBuilder
-        .forPort(port)
+    val server = ServerBuilder.forPort(port)
         .addService(object : AzEstCosmosGrpcKt.AzEstCosmosCoroutineImplBase() {
             override suspend fun estimate(req: EstimateReq) = estimateRes {
-//TODO
+//TODO サーバロジック本体
+                for (i in req.paramsList) {
+                    results.add(estResult {
+                        storageCostPerMonth = "1"
+                        throughputRequired = "2"
+                        workloadCostPerMonth = "4"
+                        totalCostPerMonth = "3"
+                    })
+                }
+            }})
+            .build()!!
+            server.start()
+            Runtime.getRuntime().addShutdownHook(
+            Thread
+            {
+                println("*** shutting down gRPC server since JVM is shutting down")
+                server.shutdown()
+                println("*** server shut down")
             }
-        })
-        .build()!!
-    server.start()
-    Runtime.getRuntime().addShutdownHook(
-        Thread {
-            println("*** shutting down gRPC server since JVM is shutting down")
-            server.shutdown()
-            println("*** server shut down")
+            )
+            println("gRPC server started on port $port.")
+            server.awaitTermination()
         }
-    )
-    println("gRPC server started on port $port.")
-    server.awaitTermination()
-}
